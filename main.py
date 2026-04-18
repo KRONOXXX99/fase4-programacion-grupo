b471e034539b05134d6c17ee897ab462f107bbdd
KRONOXXX99 <jzapatame@unadvirtual.edu.co>
2026-04-18 18:10:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from cliente import Cliente
+
 def main():
-    print("Sistema iniciado")
+    try:
+        cliente1 = Cliente("Juan", "juan@mail.com", "123456789")
+        print(cliente1.mostrar_info())
+
+        # prueba error
+        cliente2 = Cliente("", "correo_malo", "abc")
+
+    except Exception as e:
+        print(f"Error detectado: {e}")
 
 if __name__ == "__main__":
     main()
